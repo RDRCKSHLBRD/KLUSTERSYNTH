d599b0083df56c8c2dbcf1e992594d65548e919d
Roderick Shoolbraid <rdxenv@gmail.com>
2025-02-04 13:44:14 -0800

    Initial Commit

---
+++ KLUSTERSYNTH/KLUSTERSYNTHApp.swift
@@ -0,0 +1,17 @@
+//
+//  KLUSTERSYNTHApp.swift
+//  KLUSTERSYNTH
+//
+//  Created by Roderick Shoolbraid on 2025-02-04.
+//
+
+import SwiftUI
+
+@main
+struct KLUSTERSYNTHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
